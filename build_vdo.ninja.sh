a184fbc98fa8155c1f6344b6ba8370a1ccdf54eb
richard <richard.raymond@datasciences.co.za>
2023-03-22 07:52:38 +0000

    Added latest tag

--- build_vdo.ninja.sh
+++ build_vdo.ninja.sh
@@ -5,6 +5,7 @@ if [[ $version ]];
 then
     git clone https://github.com/steveseguin/vdo.ninja
     docker build -t umlatt/vdo.ninja:$version .
+    docker tag umlatt/vdo.ninja:$version umlatt/vdo.ninja:latest
 else
     printf "Version number required.\ne.g.\t$0 1.0.2\n"
 fi
