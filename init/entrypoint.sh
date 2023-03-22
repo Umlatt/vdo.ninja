acfc52aa68badae7ace3276fe6b33226f9a162ba
richard <richard.raymond@datasciences.co.za>
2023-03-22 11:08:47 +0000

    Moved to new nginx validation

--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# Configure SSL
+# Check & configure SSL
 /init/ssl_init.sh
-# Check if nginx service is running, and start if not
-echo "Validating Nginx"
-if service nginx status | grep -q "running" ; then
-    echo "Nginx service is running"
+if pidof /usr/sbin/nginx > /dev/null; then
+    echo "Nginx is running."
 else
-    echo "Nginx service is not running. Starting..."
-    service nginx start
+    echo "Nginx is starting..."
+    /usr/sbin/nginx -g "daemon off;"
 fi
-# Wait for nginx service to be killed
-while service nginx status | grep -q "running" ; do
-    sleep 5
-done
 echo "Nginx service killed. Container exiting"
