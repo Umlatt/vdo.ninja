b8c3b248eba40f6dd646749bac36a688e72acc4d
Richard Raymond <44724035+Umlatt@users.noreply.github.com>
2024-06-03 17:30:26 +0200

    Removed working & updated to 24.4

--- build_vdo.ninja.sh
+++ build_vdo.ninja.sh
@@ -1,28 +1,14 @@
-#!/bin/bash
+cd /root/dsc/vdo.ninja/
 version=$1
-github_accerss_key="github_pat_YOUR_GITHUB_KEY_HERE"
-version=$(curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $github_accerss_key" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/steveseguin/vdo.ninja/releases | jq -r '.[0].tag_name' | sed 's/v//g')
-if [[ -n $version ]];
-then
-    tag="$version-stable"
-    # Check if it is not already built
-    response=$(curl -sSL -o /dev/null -w "%{http_code}" "https://hub.docker.com/v2/repositories/umlatt/vdo.ninja/tags/$tag")
-    if [[ $response == "200" ]]; then
-        echo "Image already exists for version $tag." >> "$script_directory/build.log"
-    else
-        script_directory=$(dirname "$(readlink -f "$0")")
-        cd "$script_directory"
-        rm -rf vdo.ninja
-        git clone https://github.com/steveseguin/vdo.ninja
-        docker build -t umlatt/vdo.ninja:"$tag" .
-        if [[ $? -eq 0 ]]; then
-            docker tag umlatt/vdo.ninja:"$tag" umlatt/vdo.ninja:latest
-            docker push umlatt/vdo.ninja:"$tag" && docker push umlatt/vdo.ninja:latest
-        else
-            printf "[ERROR] Failed to build the image" > "$script_directory/error.log"
-        fi
-    fi
+skip=$2
 
+if [[ $version ]]; then
+    if [[ $skip != "-s" ]] && [[ $skip != "--skip" ]]; then
+        rm -rf vdo.ninja
+        git clone -b v24.4.stable https://github.com/steveseguin/vdo.ninja.git
+    fi
+    docker build -t umlatt/vdo.ninja:$version .
+    docker tag umlatt/vdo.ninja:$version umlatt/vdo.ninja:latest
 else
-    printf "[ERROR] Could not get the latest build version from Github." > "$script_directory/error.log"
+    printf "Version number required.\ne.g.\t$0 1.0.2\n"
 fi
